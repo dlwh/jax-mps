e392807e2809650889eb4fdd2b63856dc5d4e983
David Hall <david.hall@openathena.ai>
2026-02-07 23:07:46 -0800

    type: reject float8 in MlirTypeToMps

--- src/pjrt_plugin/type_utils.mm
+++ src/pjrt_plugin/type_utils.mm
@@ -71,6 +71,13 @@ int MpsToPjrtDtype(MPSDataType mps_type) {
 }
 
 MPSDataType MlirTypeToMps(mlir::Type type) {
+    if (auto floatType = mlir::dyn_cast<mlir::FloatType>(type)) {
+        // MPSGraph does not expose float8 datatypes.
+        // Return invalid so callers fail explicitly instead of silently widening.
+        if (floatType.getWidth() == 8)
+            return MPSDataTypeInvalid;
+    }
+
     if (type.isF32())
         return MPSDataTypeFloat32;
     if (type.isF16())
